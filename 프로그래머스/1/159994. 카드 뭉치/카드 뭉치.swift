6ccc66c4082bf67f3ba60305f57e5c096261878b
정유진 <129912074+yyujnn@users.noreply.github.com>
2024-04-17 16:04:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
@@ -1,11 +1,9 @@
 import Foundation
 
-func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
-    
+func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String { 
     var index1: Int = 0
     var index2: Int = 0
-    
-    var result: [String] = []
+
     for i in 0..<goal.count {
         if index1 < cards1.count && goal[i] == cards1[index1] {
             index1 += 1
