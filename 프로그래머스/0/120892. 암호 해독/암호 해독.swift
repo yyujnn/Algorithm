1a8b569bd6b93e3c44b4580f6711e79b9b339328
정유진 <129912074+yyujnn@users.noreply.github.com>
2025-04-01 22:48:09 +0900

    [level 0] Title: 암호 해독, Time: 1.22 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.swift
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ cipher:String, _ code:Int) -> String {
-    var count = 1
-    var result = ""
-    for i in cipher {
-        if count % code == 0 {
-            result += String(i)
-        }
-        count += 1
-    }
-    return result
+    return cipher.enumerated().filter{ ($0.offset + 1) % code == 0 }.reduce("") { $0 + String($1.element) }
 }
