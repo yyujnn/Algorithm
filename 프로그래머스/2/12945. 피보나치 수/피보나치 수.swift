61a22cecfe406288562c6a96fc1c546ca8f389c1
정유진 <129912074+yyujnn@users.noreply.github.com>
2025-04-23 23:17:20 +0900

    [level 2] Title: 피보나치 수, Time: 1.53 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,10 +1,12 @@
 func solution(_ n:Int) -> Int {
-    var nums = [0, 1]
-    
-    for i in 1..<n {
-        nums.append((nums[i] + nums[i-1]) % 1234567)
-    }
-    
-    return nums[n]
-}
+    var v1: Int = 0
+    var v2: Int = 1
 
+    for _ in 2...n {
+        let v = v1 + v2
+        v1 = v2
+        v2 = v % 1234567
+    }
+
+    return v2
+}
